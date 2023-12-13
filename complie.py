9578e61f2d44d3a949d5509e9f680a7248de09ee
ar..... <stardustlight05@gmail.com>
2023-12-13 10:14:46 +0100

    added complie.py

---
+++ complie.py
@@ -0,0 +1,25 @@
+import os
+import zipfile
+
+def zip_directory(src_dir, zip_file):
+    for root, dirs, files in os.walk(src_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            zip_file.write(file_path, os.path.relpath(file_path, os.path.join(src_dir, '..')))
+
+def create_op_file():
+    top_dir_name = os.path.basename(os.getcwd())
+    op_file_name = f"{top_dir_name}.op"
+
+    with zipfile.ZipFile(op_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zip_directory('./src', zipf)
+
+        additional_files = ['info.toml', 'LICENCE', 'README.md']
+        for file in additional_files:
+            if os.path.exists(file):
+                zipf.write(file)
+
+    print(f"Created {op_file_name} successfully.")
+
+if __name__ == "__main__":
+    create_op_file()
